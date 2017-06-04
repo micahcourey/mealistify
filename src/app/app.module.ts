0b322f8be83355ce2fc7b1d6ee10dc03184f5abd
micahcourey <micahjcourey@gmail.com>
2017-06-04 10:09:58 -0700

    Add initial component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GroceryListComponent } from './grocery-list/grocery-list.component';
+import { GroceryItemComponent } from './grocery-list/grocery-item/grocery-item.component';
+import { MealListComponent } from './meal-list/meal-list.component';
+import { DishComponent } from './meal-list/dish/dish.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryListComponent,
+    GroceryItemComponent,
+    MealListComponent,
+    DishComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
